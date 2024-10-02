b81dd34e2f625e17c3d96812e81903897cf3c6ca
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-02 21:13:11 +0200

    Add sounds

--- sound/sounds.js
+++ sound/sounds.js
@@ -1,5 +1,7 @@
 const sounds = [
   { trigger: "!ambatukam", path: "ambatukam.mp3" },
+  { trigger: "!futterluke", path: "futterluke.mp3"},
+  { trigger: "!regenwald", path: "tropischer_regenwald.mp3"}
 ];
 
 export default sounds;
