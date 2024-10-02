f17b838000023bd84ed2d82d39e467913b4e072a
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-10-02 20:43:46 +0200

    Add messages

--- messages/messages.js
+++ messages/messages.js
@@ -2,6 +2,8 @@ const messages = [
   { trigger: "dennis", response: "dennis lutsch eier" },
   { trigger: "john", response: "John ist ein Macher" },
   { trigger: "hubert", response: "Ist der SCHWULSTE HIER" },
+  { trigger: "maka", response: "Maka ist ein Knacker"},
+  { trigger: "maks", response: "Maks ist gay"},
 ];
 
 export default messages;
